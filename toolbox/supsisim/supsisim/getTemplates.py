886720b48aff38203669c85cf2152dc4f33bce2b
Stepan Pressl <pressl.stepan@gmail.com>
2025-03-07 22:28:52 +0100

    moved temporarily back to timerhook, because it is in mainline

--- toolbox/supsisim/supsisim/getTemplates.py
+++ toolbox/supsisim/supsisim/getTemplates.py
@@ -9,7 +9,7 @@ dictTemplates = {
                  'rt_nrt_iopl.tmf': embedded + fixed_step,
                  'rt.tmf' : embedded + fixed_step,
                  'fmurt.tmf' : embedded + fixed_step,
-                 'nuttx_devtimer.tmf' : embedded,
+                 'nuttx_timerhook.tmf' : embedded,
                  'nuttx_systemtickhook.tmf' : embedded,
                  'nuttx.tmf' : embedded,
                  'rt_co.tmf' :  embedded + fixed_step,
@@ -22,5 +22,23 @@ dictTemplates = {
                  'samd21.tmf' : embedded,
                  'stm32h7.tmf' : embedded,
                  }
+
+"""
+This specifies the available launch methods for some Makefile templates.
+If nothing is specified, the launch method is not defined,
+therefore the launch of the model depends on you,
+either by flashing new firmware, custom upload, etc...
+"""
+
+sim = ['Local Simulation']
+shv_update = ['SHV File Node Update']
+
+templatesLaunchMethods = {
+    'sim.tmf': sim,
+    'fmusim.tmf': sim,
+    'nuttx_devtimer.tmf': shv_update,
+    'nuttx_systemtickhook.tmf': shv_update,
+    'nuttx.tmf': shv_update,
+}
 
 
