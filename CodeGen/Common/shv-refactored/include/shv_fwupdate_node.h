41626c5ceb7374faa5c7bedbaa633887eb88efe7
Stepan Pressl <pressl.stepan@gmail.com>
2025-08-23 16:24:30 +0200

    Pysim file update node

---
+++ CodeGen/Common/shv-refactored/include/shv_fwupdate_node.h
@@ -0,0 +1,28 @@
+/*
+  COPYRIGHT (C) 2025  Stepan Pressl (press.stepan@gmail.com)
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+*/
+
+#ifndef _SHV_FWUPDATE_NODE_H
+#define _SHV_FWUPDATE_NODE_H
+
+#include <shv/tree/shv_tree.h>
+
+/* We need this visible for the STATIC tree generator */
+extern const shv_method_des_t * const shv_fwupdate_dmap_items[];
+extern const shv_dmap_t shv_fwupdate_dmap;
+
+#endif
